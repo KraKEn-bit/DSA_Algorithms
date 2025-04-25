d155cb246dd68ac8090942176088424cc5ece310
KraKEn-bit <rktaaseen@gmail.com>
2025-04-25 23:28:41 +0600

    Moore's Voting Algorithm

--- Majority_Element_MooresVotingAlgorithm.cpp
+++ Majority_Element_MooresVotingAlgorithm.cpp
@@ -29,3 +29,12 @@ int main(){
     cout<<"Majority element is: "<<ans;
     return 0;
 }
+
+//Example:
+// You're in a room with people supporting different candidates. Every time you see a person supporting someone different from your current favorite, 
+// you cancel them out — one supporter from each side walks out.The person with the most support will remain by the end.
+
+
+// If an element appears more than half the time, then:
+// 1.The count of that element is greater than the sum of all other elements combined.
+// 2.So, any "cancellation" you do between the majority and minority elements will still leave the majority element standing.
