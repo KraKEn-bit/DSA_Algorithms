01d59b9b7b21becf526c27c20b9e2476875472f1
KraKEn-bit <rktaaseen@gmail.com>
2025-05-05 13:14:48 +0600

    Row_Wise , Column_Wise and Diagonal Sum

--- Matrix-2D_Rowwise_Columnwise_Maxsum_DiagonalSum.cpp
+++ Matrix-2D_Rowwise_Columnwise_Maxsum_DiagonalSum.cpp
@@ -7,6 +7,24 @@
 
 using namespace std;
 
+void Diagonal_Sum(vector<vector<int>>&mat,int row,int col){
+    int diagonalsum=0;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(j==i){
+                diagonalsum+=mat[i][j];
+            }
+            else if(j==row-i-1){
+                diagonalsum+=mat[i][j];
+            }
+        }
+    }
+    
+    //Here the conditions are ignoring the double counted values for odd dimensioned matrix of rows and columns. suppose: 1 2 3 4 5 6 7 8 9 Here 5 
+    //comes 2 times when counting. So that 5 is counted 1 times. If I wanted two times: Condition: if(j==i || j=(row or col)-i-1) as row==col
+
+    cout<<"Diagonal sum of the matrix is: "<<diagonalsum<<endl;
+}
 void rowwise_MaxSum(vector<vector<int>>&mat,int row,int col){
     int maxrowsum=INT_MIN;
     for(int i=0;i<row;i++){
@@ -17,7 +35,6 @@ void rowwise_MaxSum(vector<vector<int>>&mat,int row,int col){
         maxrowsum=max(maxrowsum,rowsum);
     }
     cout<<"Row_Wise Max Sum is: "<<maxrowsum<<endl;
-
 }
 void columnwise_MaxSum(vector<vector<int>>&mat,int row,int col){
     int maxcolsum=INT_MIN;
@@ -42,6 +59,7 @@ int main(){
             cin>>matrix[i][j];
         }
     }
+    Diagonal_Sum(matrix,row,column);
     rowwise_MaxSum(matrix,row,column);
     columnwise_MaxSum(matrix,row,column);
     return 0;
