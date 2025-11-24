e567e0969f8f4682fccdfa68427a59c4565ff98c
KraKEn-bit <rktaaseen@gmail.com>
2025-11-24 22:44:28 +0600

    Kedane's Algorithm

--- Array/Algorithms/Kedane_Algorithm.cpp
+++ Array/Algorithms/Kedane_Algorithm.cpp
@@ -9,5 +9,28 @@
 using namespace std;
 
 int main(){
+    int numb;
+    vector<int>arr;
     
+    while (true) {
+        if (cin.peek() == '\n') {
+            cin.ignore(); 
+            break;
+        }
+        cin >> numb;
+        arr.push_back(numb);
+    }
+
+    int len=arr.size();
+
+    int currSum=0,maxSum = INT_MIN;
+
+    for(int i=0;i<len;i++){
+        currSum+=arr[i];
+        maxSum = max(maxSum,currSum);
+        if(currSum<0){
+            currSum=0;
+        }
+    }
+    cout<<maxSum;
 }
