d28e5f7204533c76b9c203637bb5944c0b6bfc22
KraKEn-bit <rktaaseen@gmail.com>
2025-12-11 12:31:22 +0600

    Finding next greater element using stack

---
+++ Stack/Next_Greater_element_using_Stack.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<string>
+#include<math.h>
+#include<limits.h>
+#include<vector>
+#include<set>
+#include<algorithm>
+#include<queue>
+#include<stack>
+
+using namespace std;
+
+int main(){
+    stack<int> st1,st2;
+    vector<int>array;
+
+    int numb;
+    while(true){
+        cin>>numb;
+        if(numb==-1){
+            break;
+        }
+        array.push_back(numb);
+        st1.push(numb);
+     }
+
+    int n = array.size();
+
+    
+    for(int i=n-1;i>=0;i--){
+        if(array[i]==st1.top()){
+            int current = st1.top();
+            int start = i+1;
+            st1.pop();
+            if(current == array[n-1]){
+                st2.push(-1);
+            }
+            for(int j=start;j<n;j++){
+                if(current<array[j]){
+                    st2.push(array[j]);
+                    break;
+                }
+                else{
+                    continue;
+                }
+            }
+        }
+    }
+
+    while(!st2.empty()){
+        cout<<st2.top()<<" ";
+        st2.pop();
+    }
+}
