3e60d431708d2320450a6ad6b1b1e1db26621ed2
KraKEn-bit <rktaaseen@gmail.com>
2025-12-24 04:12:05 +0600

    changes

---
+++ Linked_List/Deleting_a_Node.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<math.h>
+#include<limits.h>
+#include<vector>
+#include<set>
+#include<algorithm>
+#include<queue>
+#include<stack>
+
+using namespace std;
+
+class Node{
+    int data;
+    Node* next;
+    Node(int value){
+        data = value;
+        next = NULL;
+    }
+};
+
+class list {
+
+};
+
+int main(){
+
+}
