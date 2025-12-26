5f9f73b9c78d9b6fd3638e903ae20ec0d9b40a81
KraKEn-bit <rktaaseen@gmail.com>
2025-12-26 18:01:39 +0600

    Finding Middle element Then printing the rest using Linked List

---
+++ Linked_List/Middle_Element.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<string>
+#include<math.h>
+#include<limits.h>
+#include<vector>
+#include<set>
+#include<algorithm>
+#include<queue>
+#include<stack>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int value){
+        data = value;
+        next = NULL;
+    }
+};  
+
+
+class List{
+    Node* head;
+    Node* tail;
+
+    public:
+        List(){
+            head=tail=NULL;
+        }
+
+        void push_back(int value){
+            Node* newNode = new Node(value);
+
+            if(tail==NULL){
+                head=tail=newNode;
+            }
+            else{
+                tail->next = newNode;
+                tail = newNode;
+            }
+        }
+
+        void print(){
+             Node* temp = head;
+             while(temp!=NULL){
+                cout<<temp->data<<" ";
+                temp = temp->next;
+             }   
+             cout<<endl;
+        }
+
+        
+        void print_middle(){
+            Node* first=head;
+            Node* second = head;
+
+            if(head == NULL){
+                cout << "List is empty!" << endl;
+                return;
+            
+            }
+
+
+            while(second!=NULL && second->next!=NULL){
+                first = first->next;
+                second = second->next->next;
+            }
+            Node* temp = first;
+            while(temp!=NULL){
+                cout<<temp->data<<" ";
+                temp = temp->next;
+            }
+            cout<<endl;
+        }
+};
+int main(){
+    List ll;
+    int n;
+    cin>>n;
+
+    vector<int>array(n);
+
+    for(int i=0;i<n;i++){
+        cin>>array[i];
+        ll.push_back(array[i]);
+    }
+    ll.print_middle();
+}
